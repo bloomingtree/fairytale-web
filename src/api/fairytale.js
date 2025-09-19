9669b3af1a076cb39faa1d449a0cb8bbca5dce38
孙珂昌 <7988588+sun-kechang@user.noreply.gitee.com>
2025-09-19 20:03:58 +0800

    [ADD]尝试增加env

--- src/api/fairytale.js
+++ src/api/fairytale.js
@@ -1,11 +1,11 @@
 import axios from 'axios'
 
 export const getStoryList = async (page = 1, pageSize = 10) => {
-  const response = await axios.get(`/api/story?page=${page}&pageSize=${pageSize}`)
+  const response = await axios.get(import.meta.env.VITE_API_URL + `/api/story?page=${page}&pageSize=${pageSize}`)
   return response.data
 }
 
 export const getStoryDetail = async (id) => {
-  const response = await axios.get(`/api/story/${id}`)
+  const response = await axios.get(import.meta.env.VITE_API_URL + `/api/story/${id}`)
   return response.data
 }
